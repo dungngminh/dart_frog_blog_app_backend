fd5e85cf4350eaa59783493b45f2c7dd58afc7a0
dungngminh <63831488+dungngminh@users.noreply.github.com>
2023-09-11 00:01:37 +0700

    chore: move to use folder file

--- routes/api/favorites/_middleware.dart
+++ routes/api/favorites/_middleware.dart
@@ -1,5 +1,5 @@
 import 'package:dart_frog/dart_frog.dart';
-import 'package:very_good_blog_app_backend/middleware/authentication_validator.dart';
+import 'package:very_good_blog_app_backend/middleware/middleware.dart';
 
 Handler middleware(Handler handler) {
   return handler.use(authenticationValidator());
