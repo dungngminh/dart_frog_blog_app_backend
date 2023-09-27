835b7620d95d82aaeab06b17c0f056d453427c35
dungngminh <63831488+dungngminh@users.noreply.github.com>
2023-09-28 00:05:20 +0700

    chore: add cloudinary

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
+import 'package:cloudinary/cloudinary.dart';
 import 'package:dart_frog/dart_frog.dart';
 import 'package:stormberry/stormberry.dart';
+import 'package:test/test.dart';
 import 'package:very_good_blog_app_backend/models/user.dart';
 import 'package:very_good_blog_app_backend/util/jwt_handler.dart';
 
@@ -14,10 +16,17 @@ final db = Database(
   useSSL: false,
 );
 
+final cloudinary = Cloudinary.signedConfig(
+  apiKey: Platform.environment['CLOUDINARY_APIKEY'] ?? '',
+  apiSecret: Platform.environment['CLOUDINARY_APISECRET'] ?? '',
+  cloudName: Platform.environment['CLOUDNAME'] ?? '',
+);
+
 Handler middleware(Handler handler) {
   return handler
       .use(requestLogger())
       // .use(fromShelfMiddleware(enforceSSL()))
       .use(provider<Database>((_) => db))
-      .use(provider((_) => JwtHandler(userRepository: db.users)));
+      .use(provider((_) => JwtHandler(userRepository: db.users)))
+      .use(provider((_) => cloudinary));
 }
